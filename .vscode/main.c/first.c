9570abccbe9361df423e0199ba2d9faeec905b5e
Ujjwal rai <ur069119@gmail.com>
2025-02-26 11:28:15 +0530

    Delete .vscode directory

--- .vscode/main.c/first.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int num1,num2,A;
-    printf("enter two integgers -");
-    scanf(" %d %d", &num1, &num2);
-    A = num1 + num2;
-    printf("%d + %d =%d",num1,num2,A);
-    return 0;
-}
--- .vscode/main.c/test.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    printf("hlo");
-    return 0;
-}
